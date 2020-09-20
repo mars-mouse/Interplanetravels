9922e334fb6fa6cd59d972284b0201a997ac940c
Maxime Rousseau <maxime.l.rousseau@gmail.com>
2020-09-20 11:35:54 +0200

    :card_file_box: Add first Migration for the DB
    First migration to create all the tables for the newly created Entities.

---
+++ migrations/Version20200918154054.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200918154054 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, date DATETIME NOT NULL, paid_amount INT NOT NULL, promotion_value DOUBLE PRECISION NOT NULL, number_places INT NOT NULL, validated TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE bookmark (id INT AUTO_INCREMENT NOT NULL, alert TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE depart_from (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE destination (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT DEFAULT NULL, distance INT NOT NULL, distance_unit VARCHAR(2) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE destination_type (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT DEFAULT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE image_destination (id INT AUTO_INCREMENT NOT NULL, caption VARCHAR(255) DEFAULT NULL, source VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE image_transport (id INT AUTO_INCREMENT NOT NULL, caption VARCHAR(255) DEFAULT NULL, source VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE image_travel (id INT AUTO_INCREMENT NOT NULL, caption VARCHAR(255) DEFAULT NULL, source VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE itinerary (id INT AUTO_INCREMENT NOT NULL, day_arrival DATETIME NOT NULL, day_departure DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE payment (id INT AUTO_INCREMENT NOT NULL, address_billing VARCHAR(255) NOT NULL, address_delivery VARCHAR(255) DEFAULT NULL, card_number VARCHAR(255) NOT NULL, card_type VARCHAR(255) NOT NULL, crypto VARCHAR(3) NOT NULL, date_expiration DATE NOT NULL, full_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE promotion (id INT AUTO_INCREMENT NOT NULL, amount DOUBLE PRECISION NOT NULL, date_start DATETIME NOT NULL, date_end DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, comment LONGTEXT DEFAULT NULL, note INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE saved_payment (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE support_ticket (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, question_title VARCHAR(255) NOT NULL, question_full_text LONGTEXT NOT NULL, visitor_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ticket_category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE transport (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT DEFAULT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE transport_type (id INT AUTO_INCREMENT NOT NULL, description LONGTEXT DEFAULT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE travel (id INT AUTO_INCREMENT NOT NULL, date_departure DATETIME DEFAULT NULL, date_return DATETIME DEFAULT NULL, max_places INT DEFAULT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE booking');
+        $this->addSql('DROP TABLE bookmark');
+        $this->addSql('DROP TABLE depart_from');
+        $this->addSql('DROP TABLE destination');
+        $this->addSql('DROP TABLE destination_type');
+        $this->addSql('DROP TABLE image_destination');
+        $this->addSql('DROP TABLE image_transport');
+        $this->addSql('DROP TABLE image_travel');
+        $this->addSql('DROP TABLE itinerary');
+        $this->addSql('DROP TABLE payment');
+        $this->addSql('DROP TABLE promotion');
+        $this->addSql('DROP TABLE review');
+        $this->addSql('DROP TABLE saved_payment');
+        $this->addSql('DROP TABLE support_ticket');
+        $this->addSql('DROP TABLE ticket_category');
+        $this->addSql('DROP TABLE transport');
+        $this->addSql('DROP TABLE transport_type');
+        $this->addSql('DROP TABLE travel');
+        $this->addSql('DROP TABLE user');
+    }
+}
